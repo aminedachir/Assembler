314e2b60845e91b623246d7b9495b55c3e129054
aminedachir <dachiramine110@gmail.com>
2024-11-11 18:32:54 +0100

    move the value of v0 to registers

--- add_integer_with_variables.asm
+++ add_integer_with_variables.asm
@@ -2,22 +2,19 @@
 number1 :.word 
 number2: .word
 number3: .word
-
 .text
-
 li $v0, 5
 syscall 
 move $t0,$v0
 sw $t0, number1
-
 li $v0, 5
 syscall
-move $t0,$v1
+move $t1,$v0
 sw $t1, number2
-
 add $t2,$t0,$t1
 sw $t2, number3
-
 li $v0, 1
 lw $a0, number3
 syscall 
+li $v0,10
+syscall
