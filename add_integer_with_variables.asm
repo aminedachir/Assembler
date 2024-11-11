9d00db8dec1e88cff0443f4baa872acbf30caa4b
aminedachir <dachiramine110@gmail.com>
2024-11-11 14:03:21 +0100

    add sw and lw instructions

--- add_integer_with_variables.asm
+++ add_integer_with_variables.asm
@@ -1,15 +1,23 @@
 .data
-var_int :.set 
-
-
+number1 :.word 
+number2: .word
+number3: .word
 
 .text
 
 li $v0, 5
-la $a0, var_int
 syscall 
+move $t0,$v0
+sw $t0, number1
 
+li $v0, 5
+syscall
+move $t0,$v1
+sw $t1, number2
+
+add $t2,$t0,$t1
+sw $t2, number3
 
 li $v0, 1
-la $a0, var_int
+lw $a0, number3
 syscall 
