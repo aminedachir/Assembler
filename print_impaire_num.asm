56dbea6733335f7acb5d7ac6de145ce2e7738210
aminedachir <dachiramine110@gmail.com>
2024-12-02 13:55:14 +0100

    loop

---
+++ print_impaire_num.asm
@@ -0,0 +1,25 @@
+.data
+line : .asciiz "\n"
+
+.text
+
+li $t0,1
+li $t1,10
+
+loop :
+ move $a0,$t0
+ li $v0,1
+ syscall 
+ 
+ li $v0,4
+ la $a0,line
+ syscall 
+ 
+ addi $t0,$t0,2
+ 
+ ble $t0,$t1,loop
+ 
+ li $v0,10
+ syscall 
+ 
+	
